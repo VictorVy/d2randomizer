7fafee5c56670496dec36db6b1087469001b4897
Victor Yao <victoryao1039@gmail.com>
2023-05-24 00:36:46 -0700

    get weapons and armour from manifest

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,41 @@ import NavBar from "../components/NavBar";
 
 const Home = () => {
     const logged = localStorage.getItem("access_token") !== null;
+    const fetched = localStorage.getItem("weapons") !== null;
 
-    // fetch("https://www.bungie.net/Platform/Destiny2/Manifest/", {
-    //     method: "GET",
-    //     headers: {},
-    // })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         localStorage.setItem("manifest", data.Response.jsonWorldContentPaths.en);
-    //     });
+    fetch("https://www.bungie.net/Platform/Destiny2/Manifest/", {
+        method: "GET",
+        headers: {},
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            fetch("https://www.bungie.net/" + data.Response.jsonWorldContentPaths.en)
+                .then((response) => response.json())
+                .then((data) => {
+                    let keys = Object.keys(data.DestinyInventoryItemDefinition);
+
+                    const weapons = new Map();
+                    const armour = new Map();
+
+                    for (let i = 0; i < keys.length; i++) {
+                        const item = data.DestinyInventoryItemDefinition[keys[i]];
+
+                        if (item.itemType === 3) {
+                            weapons.set(item.hash, item);
+                        } else if (item.itemType === 2) {
+                            armour.set(item.hash, item);
+                        }
+                    }
+
+                    // localStorage.setItem("weapons", JSON.stringify(Array.from(weapons.entries())));
+                    // localStorage.setItem("armour", JSON.stringify(Array.from(armour.entries())));
+
+                    // console.log(localStorage.getItem("weapons"));
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        });
 
     return (
         <div className="">
